d11ae050d8b072fafede4b8583e96fa37dc1601e
kareem075 <145559971+kareem075@users.noreply.github.com>
2023-11-25 18:27:30 +0200

    Add files via upload

--- Morse Code.py
+++ Morse Code.py
@@ -6,3 +6,18 @@ MORSE_CODE_DICT = {'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f
                   'y': '-.--', 'z': '--..', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
                   '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----', ' ': ' '}
 
+def encryption(text):
+    morse_code = ''
+    for words in text.lower():
+        if words in MORSE_CODE_DICT:
+            morse_code += MORSE_CODE_DICT[words] + ' '
+    return morse_code
+
+def decryption(morse_code):
+    morse_code = morse_code.split(' ')
+    text = ''
+    for code in morse_code:
+        for key, value in MORSE_CODE_DICT.items():
+            if code == value:
+                text += key
+    return text
